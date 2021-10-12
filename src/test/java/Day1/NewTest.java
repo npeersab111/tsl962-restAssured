885c7e74b95536b7f9d635e250a8bd6eaa8ac874
npeersab111 <npeersab111@gmail.com>
2021-10-12 15:25:11 +0530

    second commit

--- src/test/java/Day1/NewTest.java
+++ src/test/java/Day1/NewTest.java
@@ -4,7 +4,10 @@ import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
-  public void f() {
+  public void f1() {
   System.out.println("hello new branch");
   }
+  public void f2() {
+	  System.out.println("hello new branch");
+	  }
 }
